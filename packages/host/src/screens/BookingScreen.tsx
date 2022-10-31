34eed286c01e1357b030e1590f86cd08c5b04f64
andrewworld <andandalex@gmail.com>
2022-10-31 10:43:45 +0100

    add placeholder

--- packages/host/src/screens/BookingScreen.tsx
+++ packages/host/src/screens/BookingScreen.tsx
@@ -1,33 +1,15 @@
 import {Federated} from '@callstack/repack/client';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import Placeholder from '../components/Placeholder';
 
 const Booking = React.lazy(() => Federated.importModule('booking', './App'));
 
 const BookingScreen = () => {
   return (
-    <React.Suspense
-      fallback={
-        <View style={styles.contentContainer}>
-          <Text style={styles.header}>Loading...</Text>
-        </View>
-      }>
+    <React.Suspense fallback={<Placeholder label="Booking" icon="calendar" />}>
       <Booking />
     </React.Suspense>
   );
 };
-
-const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  header: {
-    textAlign: 'center',
-    padding: 16,
-    fontSize: 24,
-  },
-});
 
 export default BookingScreen;

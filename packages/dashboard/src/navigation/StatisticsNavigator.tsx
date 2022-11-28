53bcc2313ff05c3c4e6eb317625feee0f354b9ef
andrewworld <andandalex@gmail.com>
2022-11-28 22:43:02 +0100

    feat: add dashboard app content

---
+++ packages/dashboard/src/navigation/StatisticsNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import NavBar from '../components/NavBar';
+import StatisticsScreen from '../screens/StatisticsScreen';
+
+export type StatisticsStackParamList = {
+  Statistics: undefined;
+};
+
+const Statistics = createNativeStackNavigator<StatisticsStackParamList>();
+
+const StatisticsNavigator = () => {
+  return (
+    <Statistics.Navigator
+      screenOptions={{
+        header: props => <NavBar {...props} />,
+      }}>
+      <Statistics.Screen name="Statistics" component={StatisticsScreen} />
+    </Statistics.Navigator>
+  );
+};
+
+export default StatisticsNavigator;

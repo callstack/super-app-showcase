f6590e5c67217fe7e6628703fc549be809030d24
andrewworld <andandalex@gmail.com>
2022-12-21 00:45:49 +0100

    delete index file from auth module

--- packages/auth/index.js
+++
@@ -1,3 +0,0 @@
-export {default as AccountScreen} from './src/screens/AccountScreen';
-export {default as SignInScreen} from './src/screens/SignInScreen';
-export {default as AuthProvider} from './src/providers/AuthProvider';

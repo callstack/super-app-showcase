6bb526f54c7c3d46c1f9002221cfd4813305e0bb
andrewworld <andandalex@gmail.com>
2023-01-08 19:33:05 +0100

    move utils to catalog server

--- packages/shopping/index.js
+++ packages/shopping/index.js
@@ -5,15 +5,16 @@
 import {ScriptManager, Script, Federated} from '@callstack/repack/client';
 import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
+import getContainersURL from '../catalog-server/utils/getContainersURL';
 import {name as appName} from './app.json';
 import {version as appVersion} from './package.json';
 
-const getContainersURL = (version, platform) => {
-  return `http://localhost:3000/${appName}?platform=${platform}&appVersion=${version}`;
-};
-
 ScriptManager.shared.addResolver(async (scriptId, caller) => {
-  const containersURL = getContainersURL(appVersion, Platform.OS);
+  const containersURL = getContainersURL({
+    version: appVersion,
+    platform: Platform.OS,
+    appName,
+  });
   const response = await fetch(containersURL);
 
   const containers = await response.json();

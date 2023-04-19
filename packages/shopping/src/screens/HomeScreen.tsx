522ba7cc75a7b43baae161c6e95f879a5f20d045
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-04-19 12:40:56 +0200

    fix: ensure every picture from picsum is different

--- packages/shopping/src/screens/HomeScreen.tsx
+++ packages/shopping/src/screens/HomeScreen.tsx
@@ -37,7 +37,7 @@ const renderProduct = ({item}: any) => (
 const renderSliderItem = () => {
   return (
     <Image
-      source={{uri: 'https://picsum.photos/600'}}
+      source={{uri: 'https://picsum.photos/600?a'}}
       style={styles.sliderItem}
     />
   );

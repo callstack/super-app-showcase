03c0e8e76864eebd67e55b8bdd149e1d34474bdf
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-10-31 12:40:01 +0100

    chore: remove index.js in auth

--- packages/auth/index.js
+++
@@ -1,5 +0,0 @@
-export * from './src/contexts/AuthContext';
-export * from './src/providers/AuthProvider';
-export * from './src/screens/AccountScreen';
-export * from './src/screens/SignInScreen';
-export * from './src/services/AuthService';

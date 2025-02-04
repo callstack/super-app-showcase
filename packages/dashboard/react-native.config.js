1208ce0f1335492ca5400113a204bebc0059f87a
szymonrybczak <szymon.rybczak@gmail.com>
2025-02-04 19:31:22 +0100

    chore: even bigger cleanup

--- packages/dashboard/react-native.config.js
+++ packages/dashboard/react-native.config.js
@@ -1,10 +1,5 @@
-const useWebpack =
-  process.env.USE_WEBPACK === 'true' || process.env.USE_WEBPACK === '1';
-
 module.exports = {
-  commands: useWebpack
-    ? require('@callstack/repack/commands/webpack')
-    : require('@callstack/repack/commands/rspack'),
+  commands: require('@callstack/repack/commands/rspack'),
   project: {
     ios: {
       automaticPodsInstallation: true,
--- packages/shopping/react-native.config.js
+++ packages/shopping/react-native.config.js
@@ -1,8 +1,3 @@
-const useWebpack =
-  process.env.USE_WEBPACK === 'true' || process.env.USE_WEBPACK === '1';
-
 module.exports = {
-  commands: useWebpack
-    ? require('@callstack/repack/commands/webpack')
-    : require('@callstack/repack/commands/rspack'),
+  commands: require('@callstack/repack/commands/rspack'),
 };
